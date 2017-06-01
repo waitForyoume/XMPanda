4c5f2111904974f5e95286dd59a0e841c4485e04
waitforyou <wait_foryoume@163.com>
2017-06-01 09:21:48 +0800

    新工程
    panda

---
+++ Panda/Panda/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Panda
+//
+//  Created by 街路口等你 on 17/6/1.
+//  Copyright © 2017年 街路口等你. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Panda/Panda/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Panda
+//
+//  Created by 街路口等你 on 17/6/1.
+//  Copyright © 2017年 街路口等你. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
